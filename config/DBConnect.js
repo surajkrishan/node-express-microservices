fdf380c1f5bcbff7a726fcb6855548863e8946ff
unknown <surajkrishan7300@gmail.com>
2020-04-18 13:32:13 +0530

    env config added

--- config/DBConnect.js
+++ config/DBConnect.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("testdb", "root", "wipro@123", {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
   operatorsAliases: false
 });
